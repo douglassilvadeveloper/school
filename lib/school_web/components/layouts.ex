c39e0af4adf68f91d8db473b03f711a7c86a359d
douglassilvadeveloper <douglassilvadeveloper@gmail.com>
2025-03-22 12:40:55 +0000

    first commit

---
+++ lib/school_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule SchoolWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use SchoolWeb, :controller` and
+  `use SchoolWeb, :live_view`.
+  """
+  use SchoolWeb, :html
+
+  embed_templates "layouts/*"
+end
